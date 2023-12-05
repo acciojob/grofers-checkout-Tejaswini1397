f8729861d5a83a67ee492d04ef8c48bf5bb89c9d
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-12-05 19:11:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
+// const getSumBtn = document.createElement("button");
+// getSumBtn.append("Get Total Price");
+// document.body.appendChild(getSumBtn);
 
-const getSum = () => {
-//Add your code here
+// const getSum = () => {
+// //Add your code here
   
-};
+// };
 
-getSumBtn.addEventListener("click", getSum);
+// getSumBtn.addEventListener("click", getSum);
 
+ const table = document.querySelector('table');
+ const lastRow = document.createElement('tr');
+ const prices = document.querySelectorAll('[data-ns-test=price]');
+ let sum = 0;
+ for(let i = 0; i < prices.length; i++) {
+  sum += parseInt(prices[i].textContent);
+ }
+ const child = document.createElement("td");
+ child.setAttribute('data-ns-test', 'grandTotal');
+
+
+ child.textContent = sum;
+ lastRow.appendChild(child);
+ table.appendChild(lastRow);
